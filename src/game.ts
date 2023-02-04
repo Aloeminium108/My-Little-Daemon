8d9e11f101b2350fc4c2b427b4d97cd3bdbdfd6b
Alice Parrish <aloeasinthevera@gmail.com>
2023-02-04 12:34:42 -0500

    Removed duplicate match3states

--- src/game.ts
+++ src/game.ts
@@ -121,19 +121,6 @@ class Game {
     initializeMinigames = () => {
         this.minigames = [
             new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
-            new Match3State(this, this.ctxSecondary, this.canvasContainer),
         ]
         this.minigames.forEach(minigame => {
             this.addState(minigame)
